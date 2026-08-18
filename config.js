7e996f9dfb7e5a0d80ee3c0a5fc029c7d03cc736
aboolrica <randy.natanael@gmail.com>
2026-08-18 20:59:15 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 // Paste the Web App URL you got from Google Apps Script deployment here.
 // It looks like: https://script.google.com/macros/s/AKfycb.../exec
-const API_URL = "PASTE_YOUR_APPS_SCRIPT_WEB_APP_URL_HERE";
+const API_URL = "https://script.google.com/macros/s/AKfycbwlD0maOS35KuBzFlSnn6ZrOQkGkLnUuZK83pZHivCF_7vsvmB5j55K8a_a3c6Q7mYo/exec";
